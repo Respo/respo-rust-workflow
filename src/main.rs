3d9509f6b0aa98484047d2b68e8033e4489386dc
tiye <jiyinyiyong@gmail.com>
2024-06-26 15:48:13 +0800

    upgrade repso; update action type

--- src/main.rs
+++ src/main.rs
@@ -26,7 +26,6 @@ struct App {
 
 impl RespoApp for App {
   type Model = Store;
-  type Action = ActionOp;
 
   fn get_store(&self) -> &Rc<RefCell<Self::Model>> {
     &self.store
@@ -38,12 +37,12 @@ impl RespoApp for App {
     APP_STORE_KEY
   }
 
-  fn dispatch(store_to_action: Rc<RefCell<Self::Model>>, op: Self::Action) -> Result<(), String> {
+  fn dispatch(store_to_action: Rc<RefCell<Self::Model>>, op: <Self::Model as RespoStore>::Action) -> Result<(), String> {
     let mut store = store_to_action.borrow_mut();
     store.update(op)
   }
 
-  fn view(store: Ref<Self::Model>) -> Result<RespoNode<Self::Action>, String> {
+  fn view(store: Ref<Self::Model>) -> Result<RespoNode<<Self::Model as RespoStore>::Action>, String> {
     let states = &store.states;
     // util::log!("global store: {:?}", store);
 
